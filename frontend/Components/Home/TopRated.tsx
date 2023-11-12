93b5f895e0a886eca2335ec40d8ad7e8e80bdc58
Sufyan Ahmed <sufyanahmed054@gmail.com>
2023-11-12 21:58:40 +0500

    ..

--- frontend/Components/Home/TopRated.tsx
+++ frontend/Components/Home/TopRated.tsx
@@ -5,9 +5,9 @@ import { motion as m } from "framer-motion";
 import { staggerContainer } from "@/utils/motion";
 function TopRated() {
   return (
-    <div className=" persecptive-container   my-80  relative  min-h-screen   ">
-      <div className=" TopRated-bg   container 2xl:w-[1400px]   gap-6 p-6   rounded-lg shadow-lg">
-        <h1 className="font-bold  text-[30px] text-center my-5 text-white">Top Sellers</h1>
+    <div className=" TopRated-bg  py-20     ">
+      <div className="   container 2xl:w-[1400px]  flex flex-col gap-6    rounded-lg shadow-lg">
+        <h1 className="font-bold  text-5xl text-center  text-white">Top Sellers</h1>
         <div className=" gap-10 flex items-center sm:justify-between  justify-center   flex-col md:flex-row">
           {topRated.map((obj) => {
             return <TopRatedCard key={obj.id} {...obj} />;
--- frontend/Components/Home/TopRatedCard.tsx
+++ frontend/Components/Home/TopRatedCard.tsx
@@ -8,7 +8,7 @@ interface TopRatedCardProps {
   category: string;
   status: string;
   img: string;
-  coverImg:string;
+  coverImg: string;
   pricing: string;
 }
 function TopRatedCard({
@@ -20,28 +20,31 @@ function TopRatedCard({
   img,
   name,
   pricing,
-  coverImg
+  coverImg,
 }: TopRatedCardProps) {
   return (
-    <div className=" flex flex-col gap-5 cursor-pointer p-2  text-white  bg-white bg-opacity-10 w-[300px] rounded-lg h-[70vh]   ">
-      <div className="w-full h-[30vh] ">
-        <img src={coverImg} alt="error" className="h-full w-full  rounded-lg" />
+    <div className=" flex flex-col  cursor-pointer p-2  shadow-lg text-dark-black  bg-[#fcfcfc]   rounded-lg    ">
+      <div className="w-full h-[200px] ">
+        <img
+          src={coverImg}
+          alt="error"
+          className="h-full w-full  rounded-lg object-cover"
+        />
       </div>
 
       <div className="flex items-center justify-between py-3 ">
-        <div className="flex items-center gap-2">
-          <img src={img} alt="error" className="h-8 w-8 object-cover" />
+        <div className="flex items-center gap-2 ">
+          <img src={img} alt="error" className="h-10 w-10 object-cover" />
           <h3 className="font-bold text-sm ">{name}</h3>
         </div>
-        <h2 className="p-2 rounded-lg border-secondary-white border-1 border-solid bg-white bg-opacity-10">
+        <h2 className="p-3 border-solid bg-purple-800 rounded-md text-white ">
           {status}
         </h2>
       </div>
-
-      <p className="text-sm font-light italic py-2">{desc}</p>
+      <h1 className="text-md font-bold">{category}</h1>
+      <p className="text-md py-2">{desc}</p>
       <div className="flex items-center justify-between md:flex-col lg:flex-row py-2">
         <div className="flex items-center gap-1 ">
-    
           <span className=" text-yellow-300">
             {" "}
             <MdOutlineStar />
