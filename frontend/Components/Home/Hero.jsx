fb01c1531abf7b4f3155091997cfbb53eab1d31f
Sufyan Ahmed <sufyanahmed054@gmail.com>
2023-12-10 20:35:28 +0500

    .

--- frontend/Components/Home/Hero.jsx
+++ frontend/Components/Home/Hero.jsx
@@ -5,13 +5,17 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "../../public/side.png";
 import Search from "../Shared/Search";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Radio, RadioGroup, Stack } from "@chakra-ui/react";
 
 const Hero = () => {
   const [input, setInput] = useState("");
   const [search, setSearch] = useState([]);
   const [valueradio, setValueradio] = useState("User");
+  const [showResult, setShowResult] = useState(false);
+  const inputRef = useRef(null);
+  const ShowRef = useRef(null);
+
   const handleInput = (e) => {
     const { value } = e.target;
     setInput(value);
@@ -23,8 +27,23 @@ const Hero = () => {
 
     const h = Search(value, valueradio);
     setSearch(h);
-    console.log(h,valueradio)
   };
+
+  useEffect(() => {
+    const removeShowResult = (e) => {
+      if (
+        inputRef.current.contains(e.target) ||
+        ShowRef.current.contains(e.target)
+      ) {
+        return;
+      }
+      setShowResult(false);
+    };
+    document.addEventListener("click", removeShowResult);
+    return () => {
+      document.removeEventListener("click", removeShowResult);
+    };
+  }, []);
   const renderSearchResults = () => {
     if (valueradio === "User") {
       return (
@@ -90,63 +109,85 @@ const Hero = () => {
 
             <div className="flex flex-col lg:flex-row gap-4 lg:gap-0">
               <input
+                ref={inputRef}
                 type="text"
                 placeholder="Block Chain Web 3.0"
                 className="text-gray-600 border-none outline-none w-[90%] py-5 px-5 rounded-sm"
                 onChange={handleInput}
+                onFocus={() => {
+                  setShowResult(true);
+                }}
               />
 
               <button className="bg-[#A835C4] w-[90%] lg:w-20 py-5 px-5 font-bold lg:flex items-center justify-center rounded-r-md bg-onlineGreen border-none cursor-pointer lg:text-xl text-3xl">
                 <FiSearch />
               </button>
             </div>
-            <div className="text-[rgb(64,65,69)] flex flex-col gap-3 bg-white h-[350px] rounded-sm w-[89%] absolute sm:top-[180px] top-[275px] shadow-xl overflow-auto  ">
-              <div className="flex justify-end px-5 py-3 ">
-                <RadioGroup onChange={setValueradio} value={valueradio}>
-                  <Stack direction="row">
-                    <Radio value="User">User</Radio>
-                    <Radio value="Category">Category</Radio>
-                    <Radio value="Subcategory">Subcategory</Radio>
-                  </Stack>
-                </RadioGroup>
+            {showResult ? (
+              <div
+                ref={ShowRef}
+                className="text-[rgb(64,65,69)] flex flex-col gap-3 bg-white h-[350px] rounded-sm w-[89%] absolute sm:top-[180px] top-[275px] shadow-xl overflow-auto  "
+              >
+                <div className="flex justify-end px-5 py-3 ">
+                  <RadioGroup onChange={setValueradio} value={valueradio}>
+                    <Stack direction="row">
+                      <Radio value="User">User</Radio>
+                      <Radio value="Category">Category</Radio>
+                      <Radio value="Subcategory">Subcategory</Radio>
+                    </Stack>
+                  </RadioGroup>
+                </div>
+
+                {search?.length > 0 && valueradio === "User" ? (
+                  <div className="flex items-center justify-between  px-5 ">
+                    <span className="font-bold">User</span>{" "}
+                    <span className="font-bold">Name</span>
+                    <span className="font-bold">Country</span>
+                  </div>
+                ) : null}
+
+                {search?.length > 0 && valueradio === "Category" ? (
+                  <div className="flex items-center justify-between px-5  ">
+                    <span className="font-bold">Category</span>{" "}
+                    <span className="font-bold">Name</span>
+                    <span className="font-bold">
+                      {" "}
+                      <span className="text-green-500 text-lg">.</span>
+                      Available
+                    </span>
+                  </div>
+                ) : null}
+
+                {search?.length > 0 && valueradio === "Subcategory px-5" ? (
+                  <div className="flex items-center justify-between ">
+                    <span className="font-bold">Fields</span>{" "}
+                    <span className="font-bold">Name</span>
+                  </div>
+                ) : null}
+
+                <div className="px-5 py-1">{renderSearchResults()}</div>
               </div>
+            ) : null}
 
-              {search?.length > 0 && valueradio === "User" ? (
-                <div className="flex items-center justify-between  px-5 ">
-                  <span className="font-bold">User</span>{" "}
-                  <span className="font-bold">Name</span>
-                  <span className="font-bold">Country</span>
-                </div>
-              ) : null}
-
-              {search?.length > 0 && valueradio === "Category" ? (
-                <div className="flex items-center justify-between px-5  ">
-                  <span className="font-bold">Category</span>{" "}
-                  <span className="font-bold">Name</span>
-                  <span className="font-bold">
-                    {" "}
-                    <span className="text-green-500 text-lg">.</span>
-                    Available
-                  </span>
-                </div>
-              ) : null}
-
-              {search?.length > 0 && valueradio === "Subcategory px-5" ? (
-                <div className="flex items-center justify-between ">
-                  <span className="font-bold">Fields</span>{" "}
-                  <span className="font-bold">Name</span>
-                </div>
-              ) : null}
-
-              <div className="px-5 py-1">{renderSearchResults()}</div>
-            </div>
             <div className="gap-4 items-center max-w-xl text-sm hidden lg:flex">
               <span className="font-bold text-md">Popular:</span>
               <Link
                 href="/"
                 className="border-[1px] border-solid border-white rounded-2xl bg-transparent py-1 px-2"
               >
-                Web Design
+                UI/UX
+              </Link>
+              <Link
+                href="/"
+                className="border-[1px] border-solid border-white rounded-2xl bg-transparent py-1 px-2"
+              >
+                AI SERVICES
+              </Link>
+              <Link
+                href="/"
+                className="border-[1px] border-solid border-white rounded-2xl bg-transparent py-1 px-2"
+              >
+                Web Development
               </Link>
             </div>
           </div>
