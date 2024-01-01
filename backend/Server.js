6a8361e73d1b03b13c00eb730891bdeec310d117
Sufyan Ahmed <sufyanahmed054@gmail.com>
2024-01-01 15:42:36 +0500

    ..

---
+++ backend/Server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const server = require("http").createServer(app);
+const userRoutes = require("./Routes/userRoute");
+dotenv.config();
+const corsOption = {
+  origin: "http://localhost:3000",
+  credentials: true,
+  exposedHeaders: ["set-cookie"],
+};
+require("./DB/connection");
+app.use(cors(corsOption));
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/v1", userRoutes);
+server.listen(process.env.PORT || 3001, () => {
+  console.log(`Server Running on ${process.env.PORT} Port`);
+});
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Features, Hero, TopRated, PlatformFeatures } from "../Components";
-export default  function Home() {
-
+export default function Home() {
   return (
     <main className="min-h-screen bg-dark-black overflow-hidden">
       <Hero />
