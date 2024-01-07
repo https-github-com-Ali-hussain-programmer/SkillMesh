0f9894da7c692f142705e463a2d5a846afd20e45
Sufyan Ahmed <sufyanahmed054@gmail.com>
2024-01-07 20:04:33 +0500

    ..

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,10 @@
-"use client";
-import { useEffect } from "react";
+import { Metadata } from "next";
 import { Features, Hero, TopRated, PlatformFeatures } from "../Components";
+export const metadata: Metadata = {
+  title: "SkillMesh |Web3 FrreLancing Dashboard Template",
+  description: "This is Home Blog page for TailAdmin Next.js",
+};
 export default function Home() {
-
   return (
     <main
       className={`min-h-screen bg-dark-black
