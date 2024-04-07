6980fdd225a04a7861a126e5730456782fb2487d
SufyanAhmedDafi <sufyanahmed054@gmail.com>
2024-04-08 00:58:31 +0500

    ..

---
+++ frontend/middleware.js
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  const cookies = request.cookies;
+  const token = cookies.get("token");
+  if (!token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/profile",
+    "/Payment",
+    "/Payment/ConfirmPay",
+    "/Payment/SubmitRequirement",
+  ],
+};
